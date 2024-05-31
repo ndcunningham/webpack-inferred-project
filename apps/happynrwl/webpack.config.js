0082fc25b1afe64e0dcb3b17ab2257c1d513386c
Nicholas Cunningham <ndcunningham@gmail.com>
2024-05-31 14:15:25 -0600

    migrate webpack to use build as inferred target

--- apps/happynrwl/webpack.config.js
+++ apps/happynrwl/webpack.config.js
@@ -1,17 +1,30 @@
-const { composePlugins, withNx } = require('@nx/webpack');
-const { withReact } = require('@nx/react');
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { join } = require('path');
 
-// Nx plugins for webpack.
-module.exports = composePlugins(
-  withNx(),
-  withReact({
-    // Uncomment this line if you don't want to use SVGR
-    // See: https://react-svgr.com/
-    // svgr: false
-  }),
-  (config) => {
-    // Update the webpack config as needed here.
-    // e.g. `config.plugins.push(new MyPlugin())`
-    return config;
-  }
-);
+// q: How do i add the type webpack config?
+// a: You can add the type to the module.exports like this:
+// module.exports: import { Configuration } from 'webpack';
+/**
+ * @type {import('webpack').WebpackOptionsNormalized}
+ */
+
+let config = {
+  output: {
+    // Relative to projectRoot
+    path: join(__dirname, '../../dist/apps/happynrwl'),
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      compiler: 'babel',
+      index: './src/index.html',
+      baseHref: '/',
+      main: './src/main.tsx',
+      tsConfig: './tsconfig.app.json',
+      assets: ['./src/favicon.ico', './src/assets'],
+      styles: ['./src/styles.scss'],
+      scripts: [],
+    }),
+  ],
+};
+
+module.exports = config;
